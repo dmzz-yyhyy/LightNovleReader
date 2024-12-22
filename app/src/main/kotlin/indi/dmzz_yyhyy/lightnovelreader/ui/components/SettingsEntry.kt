ba95513d4881ffb5537156323bc66615770be515
yukonisen <whenten99@gmail.com>
2024-12-22 22:20:11 +0800

    实现设置页新UI

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/components/SettingsEntry.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/components/SettingsEntry.kt
@@ -2,15 +2,24 @@ package indi.dmzz_yyhyy.lightnovelreader.ui.components
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
@@ -23,9 +32,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
@@ -38,6 +52,7 @@ import kotlin.math.roundToInt
 @Composable
 fun SettingsSwitchEntry(
     modifier: Modifier = Modifier,
+    iconRes: Int = -1,
     title: String,
     description: String,
     checked: Boolean,
@@ -46,6 +61,7 @@ fun SettingsSwitchEntry(
 ) {
     SettingsSwitchEntry(
         modifier = modifier,
+        iconRes = iconRes,
         title = title,
         description = description,
         checked = checked,
@@ -57,46 +73,69 @@ fun SettingsSwitchEntry(
 @Composable
 fun SettingsSwitchEntry(
     modifier: Modifier = Modifier,
+    iconRes: Int,
     title: String,
     description: String,
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit,
     disabled: Boolean = false
 ) {
-    FilledCard(
+    Row(
         modifier = modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(6.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(6.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+            .wrapContentHeight()
+            .then(if (!disabled) Modifier.clickable { onCheckedChange(!checked) } else Modifier)
+            .padding(start = 18.dp, end = 14.dp)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .then(if (disabled) Modifier.clickable {} else Modifier.clickable { onCheckedChange(!checked) })
-            .padding(18.dp, 10.dp, 20.dp, 12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(
-                Modifier
-                    .weight(2f)
-                    .padding(end = 4.dp)
+        if (iconRes > 0) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(end = 8.dp),
+                contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.W500,
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    maxLines = 1
-                )
-                Text(
-                    text = description,
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.W500,
-                    fontSize = 13.sp,
-                    lineHeight = 14.sp,
-                    color = MaterialTheme.colorScheme.onSurface
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(iconRes),
+                    tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                    contentDescription = "Icon"
                 )
             }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 16.sp,
+                lineHeight = 16.sp
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = description,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 14.sp,
+                lineHeight = 18.sp
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ) {
             Switch(
                 checked = checked,
                 enabled = !disabled,
@@ -108,8 +147,9 @@ fun SettingsSwitchEntry(
 
 @Composable
 fun SettingsSliderEntry(
+    iconRes: Int = -1,
     modifier: Modifier = Modifier,
-    description: String,
+    title: String,
     unit: String,
     value: Float,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
@@ -120,8 +160,9 @@ fun SettingsSliderEntry(
         tempValue = value
     }
     SettingsSliderEntry(
+        iconRes = iconRes,
         modifier = modifier,
-        description = description,
+        title = title,
         unit = unit,
         value = tempValue,
         valueRange = valueRange,
@@ -131,35 +172,63 @@ fun SettingsSliderEntry(
 }
 
 @Composable
-fun SettingsSliderEntry(
+private fun SettingsSliderEntry(
     modifier: Modifier = Modifier,
-    description: String,
+    iconRes: Int,
+    title: String,
     unit: String,
     value: Float,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
     onSlideChange: (Float) -> Unit,
     onSliderChangeFinished: () -> Unit
 ) {
-    FilledCard(
-        modifier = modifier,
-        shape = RoundedCornerShape(6.dp)
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(6.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+            .wrapContentHeight()
+            .padding(start = 18.dp, end = 14.dp)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        Column(Modifier.padding(18.dp, 10.dp, 20.dp, 12.dp)) {
+        if (iconRes > 0) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(end = 8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(iconRes),
+                    tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                    contentDescription = "Icon"
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
             Text(
-                text = description,
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.W500,
-                fontSize = 18.sp,
+                text = title,
                 color = MaterialTheme.colorScheme.onSurface,
-                maxLines = 1
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 16.sp,
+                lineHeight = 16.sp
             )
+            Spacer(modifier = Modifier.height(2.dp))
             Text(
                 text = "${DecimalFormat("#.#").format(value)}$unit",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.W500,
-                fontSize = 13.sp,
-                lineHeight = 14.sp,
-                color = MaterialTheme.colorScheme.secondary,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
                 maxLines = 1
             )
             Slider(
@@ -176,54 +245,84 @@ fun SettingsSliderEntry(
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SettingsMenuEntry(
+    iconRes: Int = -1,
     title: String,
-    description: String,
+    description: String? = null,
     options: MenuOptions,
     selectedOptionKey: String,
     onOptionChange: (String) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
 
-    FilledCard(
+    Row(
         modifier = Modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(6.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(6.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+            .wrapContentHeight()
+            .pointerInteropFilter {
+                offset = Offset(it.x, it.y); false
+            }
+            .clickable { expanded = !expanded }
+            .padding(start = 18.dp, end = 14.dp)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        Box(modifier = Modifier.clickable { expanded = !expanded }) {
-            Row(
+        if (iconRes > 0) {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(18.dp, 10.dp, 20.dp, 12.dp),
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxHeight()
+                    .padding(end = 8.dp),
+                contentAlignment = Alignment.Center
             ) {
-                Column(
-                    Modifier.weight(2f)
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.W500,
-                        fontSize = 18.sp,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        maxLines = 1
-                    )
-                    Column {
-                        Text(
-                            text = description,
-                            fontSize = 13.sp,
-                            lineHeight = 14.sp,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                        AnimatedText(
-                            text = options.get(selectedOptionKey).name,
-                            fontSize = 13.sp,
-                            lineHeight = 14.sp,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                    }
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(iconRes),
+                    tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                    contentDescription = "Icon"
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 16.sp,
+                lineHeight = 16.sp
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            description?.let {
+                Text(
+                    text = description,
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 14.sp,
+                    lineHeight = 18.sp
+                )
+            }
+            AnimatedText(
+                text = options.get(selectedOptionKey).name,
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Box(
+                modifier = Modifier.offset {
+                    IntOffset(offset.x.toInt(), 0)
                 }
+            ) {
                 DropdownMenu(
                     expanded = expanded,
                     onDismissRequest = { expanded = false }
@@ -234,23 +333,24 @@ fun SettingsMenuEntry(
                                 onOptionChange(option.key)
                                 expanded = false
                             },
-                            text = { Text(option.name) },
+                            text = { Text(option.name, fontSize = 14.sp) },
                         )
                     }
                 }
             }
         }
-
     }
 }
 
 @Composable
 fun SettingsClickableEntry(
+    iconRes: Int = -1,
     title: String,
     description: String,
     option: String? = null
 ) {
     SettingsClickableEntry(
+        iconRes = iconRes,
         title = title,
         description = description,
         option = option,
@@ -260,6 +360,7 @@ fun SettingsClickableEntry(
 
 @Composable
 fun SettingsClickableEntry(
+    iconRes: Int = -1,
     title: String,
     description: String,
     option: String? = null,
@@ -267,6 +368,7 @@ fun SettingsClickableEntry(
 ) {
     val context = LocalContext.current
     SettingsClickableEntry(
+        iconRes = iconRes,
         title = title,
         description = description,
         option = option,
@@ -281,59 +383,61 @@ fun SettingsClickableEntry(
 
 @Composable
 fun SettingsClickableEntry(
+    iconRes: Int = -1,
     title: String,
     description: String,
     option: String? = null,
     onClick: () -> Unit
 ) {
-    var expanded by remember { mutableStateOf(false) }
-
-    FilledCard(
+    Row(
         modifier = Modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(6.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(6.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+            .wrapContentHeight()
+            .clickable { onClick.invoke() }
+            .padding(start = 18.dp, end = 14.dp)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        Box(
-            modifier = Modifier.clickable {
-                expanded = !expanded
-                onClick.invoke()
-            }
-        ) {
-            Row(
+        if (iconRes > 0) {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(18.dp, 10.dp, 20.dp, 12.dp),
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxHeight()
+                    .padding(end = 8.dp),
+                contentAlignment = Alignment.Center
             ) {
-                Column(
-                    Modifier.weight(2f)
-                ) {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.W500,
-                        fontSize = 18.sp,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        maxLines = 1
-                    )
-                    Column {
-                        Text(
-                            text = description,
-                            fontSize = 13.sp,
-                            lineHeight = 14.sp,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                        option?.let {
-                            Text(
-                                text = it,
-                                fontSize = 13.sp,
-                                lineHeight = 14.sp,
-                                color = MaterialTheme.colorScheme.primary
-                            )
-                        }
-                    }
-                }
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(iconRes),
+                    tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                    contentDescription = "Icon"
+                )
             }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 16.sp,
+                lineHeight = 16.sp
+            )
+            Spacer(modifier = Modifier.height(2.dp))
+            Text(
+                text = description,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 14.sp,
+                lineHeight = 18.sp
+            )
         }
     }
 }

99810c55fa197e76c405624960a9192e8e32e56b
NightFish <hk198580666@outlook.com>
2025-01-13 03:12:39 +0800

    完成epub导出功能 #131

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/book/detail/DetailViewModel.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/book/detail/DetailViewModel.kt
@@ -1,18 +1,27 @@
 package indi.dmzz_yyhyy.lightnovelreader.ui.book.detail
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import dagger.hilt.android.lifecycle.HiltViewModel
 import indi.dmzz_yyhyy.lightnovelreader.data.BookRepository
 import indi.dmzz_yyhyy.lightnovelreader.data.bookshelf.BookshelfRepository
-import javax.inject.Inject
+import indi.dmzz_yyhyy.lightnovelreader.data.work.ExportBookToEPUBWork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val bookRepository: BookRepository,
-    private val bookshelfRepository: BookshelfRepository
+    private val bookshelfRepository: BookshelfRepository,
+    private val workManager: WorkManager,
 ) : ViewModel() {
     private val _uiState = MutableDetailUiState()
     val uiState: DetailUiState = _uiState
@@ -40,5 +49,22 @@ class DetailViewModel @Inject constructor(
                 _uiState.userReadingData = it
             }
         }
+    }
+
+    fun exportToEpub(uri: Uri, bookId: Int): Flow<WorkInfo> {
+        val workRequest = OneTimeWorkRequestBuilder<ExportBookToEPUBWork>()
+            .setInputData(
+                workDataOf(
+                    "bookId" to bookId,
+                    "uri" to uri.toString(),
+                )
+            )
+            .build()
+        workManager.enqueueUniqueWork(
+            bookId.toString(),
+            ExistingWorkPolicy.KEEP,
+            workRequest
+        )
+        return workManager.getWorkInfoByIdFlow(workRequest.id)
     }
 }
